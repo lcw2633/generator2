4a07ed99520fababfb23994534653f0a6d2a9813
tom_glb <goodmorning_glb@atguigu.com>
2021-10-04 17:37:17 +0800

    first commit

---
+++ src/test/java/com/xxxx/generator/Test.java
@@ -0,0 +1,19 @@
+package com.xxxx.generator;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Node.insert(new Node(1));
+        Node.insert(new Node(2));
+        Node.insert(new Node(3));
+        Node.insert(new Node(4));
+        Node.insert(new Node(5));
+        Node.insert(new Node(6));
+        //Node.bl(Node.headNode);
+
+        Node node = Node.reverse(Node.headNode);
+
+        Node.bl(node);
+
+    }
+}
